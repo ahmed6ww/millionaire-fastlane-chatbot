ff9e47808dbc6461318a88745b036200f6c3579d
Ahmed <heydude08080@gmail.com>
2024-11-22 03:29:54 +0500

    resolve error

---
+++ millionaire_fastlane_chatbot/configuration.py
@@ -0,0 +1,17 @@
+from pinecone import Pinecone, ServerlessSpec
+import os
+
+key = os.getenv("PINECONE_API_KEY")
+pc = Pinecone(api_key=key)
+# Create a serverless index
+index_name = "millionairefastlanechatbot"
+
+pc.create_index(
+    name=index_name,
+    dimension=768, # Replace with your model dimensions
+    metric="cosine", # Replace with your model metric
+    spec=ServerlessSpec(
+        cloud="aws",
+        region="us-east-1"
+    ) 
+)
